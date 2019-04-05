68929278e24618f8092f8481e56fef133c4cf8e6
Oleh SAMOILENKO <osamoile@e1r11p6.unit.ua>
2019-04-05 12:58:31 +0300

    _.cor

--- vm/bin/all.sh
+++ vm/bin/all.sh
@@ -1,38 +1,38 @@
 list=(
-# 42.cor
-Asombra.cor
-Bazou.cor
-# Car.cor # very long
-Code_eater.cor
-# Douceur_power.cor # long
-Eraser.cor
-Explosive_Kitty.cor
-# ForgottenOne.cor # OK
-Gagnant.cor
-Hidden.cor			
-# Horace.cor			# OK
-# Izi_Miaou.cor		# OK
-Kappa.cor			# 	SF
-# Kitty_Miaou.cor		# OK
-# Kittystrophic.cor	# OK
-# Machine-gun.cor		# OK
-# MarineKing.cor		# OK
-# Misaka_Mikoto.cor	# OK
-NoIdea.cor			# 	SF
-# O-Maley_Miaou.cor
-# Octobre_Rouge_V4.2.cor
-# Rainbow_dash.cor
-# Stargate0_1.cor
-# THUNDER.cor
-# TheHarvester.cor
-# Varimathras.cor
-# Wall.cor
-# _.cor
-# _honeybadger.cor
-# barriere.cor
-# bee_gees.cor
+# 42.cor					#
+# Asombra.cor				# OK
+# Bazou.cor					# OK
+# Car.cor					# very long
+# Code_eater.cor			# OK
+# Douceur_power.cor			# long
+# Eraser.cor				# OK
+# Explosive_Kitty.cor		# OK
+# ForgottenOne.cor			# OK
+# Gagnant.cor				# OK
+# Hidden.cor				# OK
+# Horace.cor				# OK
+# Izi_Miaou.cor				# OK
+# Kappa.cor					# OK
+# Kitty_Miaou.cor			# OK
+# Kittystrophic.cor			# OK
+# Machine-gun.cor			# OK
+# MarineKing.cor			# OK
+# Misaka_Mikoto.cor			# OK
+# NoIdea.cor				# Seg Fault
+# O-Maley_Miaou.cor			# OK
+# Octobre_Rouge_V4.2.cor	# long
+# Rainbow_dash.cor			# OK
+# Stargate0_1.cor			# OK
+# THUNDER.cor				# OK
+# TheHarvester.cor			# OK
+# Varimathras.cor			# OK
+# Wall.cor					# OK
+# _.cor						# OK
+# _honeybadger.cor			# OK
+# barriere.cor				
+# bee_gees.cor				
 # big_feet.cor
-bigzork.cor
+bigzork.cor				# OK
 # casimir.cor
 # champ.cor
 # corelol.cor
@@ -40,14 +40,14 @@ bigzork.cor
 # doge.cor
 # dubo.cor
 # echo.cor
-ex.cor
+ex.cor					# OK
 # fast.cor
-fluttershy.cor
+fluttershy.cor			# OK
 # gateau.cor
 # gedeon.cor
 # helltrain.cor
 # jinx.cor
-jumper.cor
+jumper.cor				# OK
 # justin_bee.cor
 # kamelkaze.cor
 # katchup.cor
@@ -62,7 +62,7 @@ jumper.cor
 # maxidef.cor
 # meowluigi.cor
 # mise_a_jour_windows95.cor
-mortel.cor
+mortel.cor				# OK
 # overwatch.cor
 # pai_mu_tan.cor
 # run_Kitty_RUN.cor
@@ -73,16 +73,16 @@ mortel.cor
 # slider2.cor
 # tchupka.cor
 # terminator.cor
-toto.cor
-turtle.cor
+toto.cor					# OK
+turtle.cor				# OK
 # ultima.cor
 # ultimate-surrender.cor
 # wave.cor
 # youforkmytralala.cor
-zork.cor
+zork.cor					# OK
 )
 
 for i in ${list[@]}; do
 	echo "\t"$i
 	sh bin/bool.sh resources/champs/cor/$i
-done
+done;
