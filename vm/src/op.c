dea334d2518e0deaefe6d72bccadac5b437aa012
Oleh SAMOILENKO <osamoile@e1r11p10.unit.ua>
2019-04-11 15:13:25 +0300

    op.c

--- vm/src/op.c
+++ vm/src/op.c
@@ -398,24 +398,3 @@ void	op_aff(t_carriage *car, t_war *war)
 	}
 }
 
-t_op		op_tab[] =  // [17]
-{
-//   name       args                                                                      code cycles codage label
-	{ "live",	1,	{					T_DIR,						0,				0	},	 1,	  10,	0,	0,	 &op_live	},
-	{   "ld",	2,	{			T_DIR | T_IND,					T_REG,				0	},	 2,	   5,	1,	0,	   &op_ld	},
-	{   "st",	2,	{					T_REG,			T_IND | T_REG,				0	},	 3,	   5,	1,	0,	   &op_st	},
-	{  "add",	3,	{					T_REG,					T_REG,			T_REG	},	 4,	  10,	1,	0,	  &op_add	},
-	{  "sub",	3,	{					T_REG,					T_REG,			T_REG	},	 5,	  10,	1,	0,	  &op_sub	},
-	{  "and",	3,	{	T_REG | T_IND | T_DIR,	T_REG | T_IND | T_DIR,			T_REG	},	 6,	   6,	1,	0,	  &op_and	},
-	{   "or",	3,	{	T_REG | T_IND | T_DIR,	T_REG | T_IND | T_DIR,			T_REG	},	 7,	   6,	1,	0,	   &op_or	},
-	{  "xor",	3,	{	T_REG | T_IND | T_DIR,	T_REG | T_IND | T_DIR,			T_REG	},	 8,	   6,	1,	0,	  &op_xor	},
-	{ "zjmp",	1,	{					T_DIR,						0,				0	},	 9,	  20,	0,	1,	 &op_zjmp	},
-	{  "ldi",	3,	{	T_REG | T_DIR | T_IND,			T_DIR | T_REG,			T_REG	},	10,	  25,	1,	1,	  &op_ldi	},
-	{  "sti",	3,	{					T_REG,	T_REG | T_DIR | T_IND,	T_DIR | T_REG	},	11,	  25,	1,	1,	  &op_sti	},
-	{ "fork",	1,	{					T_DIR,						0,				0	},	12,	 800,	0,	1,	 &op_fork	},
-	{  "lld",	2,	{			T_DIR | T_IND,					T_REG,				0	},	13,	  10,	1,	0,	  &op_lld	},
-	{ "lldi",	3,	{	T_REG | T_DIR | T_IND,			T_DIR | T_REG,			T_REG	},	14,	  50,	1,	1,	 &op_lldi	},
-	{"lfork",	1,	{					T_DIR,						0,				0	},	15,	1000,	0,	1,	&op_lfork	},
-	{  "aff",	1,	{					T_REG,						0,				0	},	16,	   2,	1,	0,	  &op_aff	},
-	{      0,	0,	{						0,						0,				0	},	 0,	   0,	0,	0,			0	}
-};
