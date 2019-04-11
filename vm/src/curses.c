93361e4a3ea6aa1bd79a0a2dab809c8944a0a621
Vadim BROVENKO <vbrovenk@e1r11p9.unit.ua>
2019-04-11 19:42:43 +0300

    norm curses

--- vm/src/curses.c
+++ vm/src/curses.c
@@ -12,103 +12,7 @@
 
 #include "vm.h"
 
-void	print_info(t_war *war)
-{
-	char *itoa;
-
-	wresize(war->win_mem, 68, 197);
-	wresize(war->win_info, 68, 60);
-	wattron(war->win_mem, COLOR_PAIR(15));
-	wattron(war->win_info, COLOR_PAIR(15));
-	box(war->win_mem, '*', '*');
-	box(war->win_info, '*', '*');
-	wattroff(war->win_mem, A_COLOR);
-	wattroff(war->win_info, A_COLOR);
-
-	if (war->flag_run == false)
-		mvwaddstr(war->win_info, 2, 3, "** STOP **");
-	else
-		mvwaddstr(war->win_info, 2, 3, " ** RUN **");
-
-	mvwprintw(war->win_info, 4, 3, "Cycles in second : %d  ", war->cycles_in_second);
-
-	mvwprintw(war->win_info, 7, 3, "Cycle : %d", war->cycle);
-	mvwprintw(war->win_info, 9, 3, "Processes : %d             ", war->processes_amount);
-
-	wattron(war->win_info, A_BOLD);
-	int player = 0;
-	int line = 7;
-	while (player < 4)
-	{
-		if (war->champs[player] != NULL)
-		{
-			wattroff(war->win_info, A_COLOR);
-			mvwprintw(war->win_info, line + 4 * war->champs[player]->number, 3, "Player -%d : ", war->champs[player]->number);
-			wattron(war->win_info, COLOR_PAIR(war->champs[player]->number));
-			char *sub_name = ft_strsub(war->champs[player]->header->prog_name, 0, 41);
-			mvwaddstr(war->win_info, line + 4 * war->champs[player]->number, 15, sub_name);
-			ft_strdel(&sub_name);
-			// line += 4;
-			wattroff(war->win_info, A_COLOR);		
-		}
-		player++;
-	}
-
-	 player = 0;
-	 line = 7;
-	while (player < 4)
-	{
-		if (war->champs[player] != NULL)
-		{
-			mvwprintw(war->win_info, line + 4 * war->champs[player]->number + 1, 5, "Last live : %21d", war->champs[player]->last_live);
-			mvwprintw(war->win_info, line + 4 * war->champs[player]->number + 2, 5, "Lives in current period : %7d", war->champs[player]->lives_cur_period);
-			if (line + 4 * war->champs[player]->number + 2 > war->last_line)
-				war->last_line = line + 4 * war->champs[player]->number + 2;
-		}
-		player++;
-	}
-	mvwprintw(war->win_info, war->last_line + 2, 3, "CYCLE_TO_DIE : %d              ", war->cycles_to_die);
-	wrefresh(war->win_info);
-}
-
-void	print_memory(t_war *war)
-{
-	int i = -1;
-
-	while (++i < MEM_SIZE)
-	{
-		// color on
-		if (war->map[i]->color == 0)
-		{
-			wattron(war->win_mem, A_BOLD);
-			wattron(war->win_mem, COLOR_PAIR(14));
-		}
-		else
-			wattron(war->win_mem, COLOR_PAIR(war->map[i]->color));
-		if (war->map[i]->cycles_bold && war->cycle - war->map[i]->cycles_bold < 50 - 1) 
-			wattron(war->win_mem, A_BOLD);
-		if (war->map[i]->cycles_live && war->cycle - war->map[i]->cycles_live < 50 - 1) // 50 ?
-		{
-			wattron(war->win_mem, A_BOLD);
-			wattron(war->win_mem, COLOR_PAIR(8 + war->map[i]->color));
-		}
-		mvwprintw(war->win_mem, i / 64 + 2, (i % 64) * 3 + 3, "%02x", war->map[i]->value);
-		wattroff(war->win_mem, A_BOLD);
-	}
-	t_carriage *tmp = war->carriages; // need?
-	while (tmp)
-	{
-		if (war->map[tmp->position]->color == 0)
-			wattron(war->win_mem, COLOR_PAIR(15));
-		else
-			wattron(war->win_mem, COLOR_PAIR(4 + war->map[tmp->position]->color)); // number
-		mvwprintw(war->win_mem, tmp->position / 64 + 2, (tmp->position % 64) * 3 + 3, "%02x", war->map[tmp->position]->value);
-		tmp = tmp->next;
-	}
-	wrefresh(war->win_mem);
-}
-
-void	define_colors()
+void	define_colors(void)
 {
 	start_color();
 	init_pair(1, COLOR_GREEN, COLOR_BLACK);
@@ -154,14 +58,16 @@ void	over_over(t_war *war)
 void	over_curses(t_war *war)
 {
 	int key;
+
+	print_memory(war);
+	print_info(war);
 	mvwaddstr(war->win_info, war->last_line + 4, 3, "* WINNER * - ");
 	wattron(war->win_info, COLOR_PAIR(war->last_live->number));
-	mvwaddstr(war->win_info, war->last_line + 4, 16, war->last_live->header->prog_name);
+	mvwaddstr(war->win_info, war->last_line + 4, 16,
+								war->last_live->header->prog_name);
 	wattroff(war->win_info, A_COLOR);
 	wrefresh(war->win_info);
 	while ((key = getch()) != KEY_ESC)
-	{
-
-	}
+		;
 	over_over(war);
 }
