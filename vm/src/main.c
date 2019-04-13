6f5c9d65d51f15aaae17c88db9e5e7742dfe7e0b
Oleh SAMOILENKO <osamoile@e1r10p9.unit.ua>
2019-04-13 13:01:56 +0300

    vm norme ok

--- vm/src/main.c
+++ vm/src/main.c
@@ -37,15 +37,15 @@ void	error(char *message)
 {
 	ft_putstr_fd("Error: ", 2);
 	ft_putendl_fd(message, 2);
-	system("leaks corewar | grep 'leaked bytes'");
+	system("leaks corewar");
 	exit(0);
 }
 
 void	usage(void)
 {
 	ft_printf("Usage: ./corewar <[-n number] champion1.cor> <...> [-verbose] \
-									[-visual] [-dev] [-dump cycle_number]\n");
-	system("leaks corewar | grep 'leaked bytes'");
+[-visual] [-dump cycle_number]\n");
+	system("leaks corewar");
 	exit(0);
 }
 
@@ -74,5 +74,6 @@ int		main(int argc, char **argv)
 				dump(war);
 		}
 	over_curses(war);
+	system("leaks corewar");
 	return (0);
 }
