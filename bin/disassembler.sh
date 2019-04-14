59a2c57f5995667f6e5ee2bd9d89977828f9fe27
Oleh SAMOILENKO <osamoile@e1r9p15.unit.ua>
2019-04-14 11:41:21 +0300

    fix script

--- bin/disassembler.sh
+++ bin/disassembler.sh
@@ -2,14 +2,15 @@ if [[ $1 == "" ]]
 then
 	echo "Usage: sh bool.sh CHAMPION"
 else
-	./asm/asm -d $1 >&-
+	cp $1 bin
 	filename=$(basename $1 .cor)
-	mv resources/champs/cor/$filename.s resources/champs/dis_test
-	./asm/asm resources/champs/dis_test/$filename.s >&-
-	if [ "$(diff -u $1 resources/champs/dis_test/$filename.cor)" == "" ]
+	./asm/asm -d bin/$filename.cor
+	./asm/asm bin/$filename.s
+	if [ "$(diff -u $1 bin/$filename.cor)" == "" ]
 	then
 		echo "\033[1;32mOK\033[0m"
 	else
 		echo "\033[31mKO\033[0m"
 	fi
+	rm bin/$filename.cor bin/$filename.s
 fi
